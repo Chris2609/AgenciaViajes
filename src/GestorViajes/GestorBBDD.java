0dffa9c443a7bd7a0d223aafa8d3af1c1bc0ce5c
Chris2609 <megustas260904@gmail.com>
2023-02-07 08:58:20 +0100

    clase gestorBBDD con metodo para insertarcliente

---
+++ src/GestorViajes/GestorBBDD.java
@@ -0,0 +1,29 @@
+package GestorViajes;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class GestorBBDD {
+
+	Conector con = new Conector();
+	
+	public void insertarCliente(Cliente cliente) {
+		con.conectar();
+		
+        try {
+			PreparedStatement insertarC = con.getConexion().prepareStatement("INSERT INTO clientes (dni, nombre, apellidos, direccion, localidad) VALUES (?,?,?,?,?)");
+			insertarC.setString(1, cliente.getDni());
+			insertarC.setString(2, cliente.getNombre());
+			insertarC.setString(3, cliente.getApellido());
+			insertarC.setString(4, cliente.getDireccion());
+			insertarC.setString(5, cliente.getLocalidad());
+			
+			insertarC.execute();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+}
