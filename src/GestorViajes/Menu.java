289ff80936a24e52f493d680288f89836b657306
Chris2609 <megustas260904@gmail.com>
2023-02-16 12:13:20 +0100

    menu con opcion para consultar reservas entre fechas

--- src/GestorViajes/Menu.java
+++ src/GestorViajes/Menu.java
@@ -24,11 +24,12 @@ public class Menu {
 	public static final int MOSTRAR_CLIENTECON = 6;
 
 	public static void menuGestorResevas() {
-		System.out.println("1. Realizar reserva\n2. Anular reserva\n0. Salir");
+		System.out.println("1. Realizar reserva\n2. Anular reserva\n3. Consultar reservas entre fechas\n0. Salir");
 	}
 	
 	public static final int REALIZAR_RESERVA = 1;
 	public static final int ANULAR_RESERVA = 2;
+	public static final int CONSULTAR_RESERVAS_FECHAS = 3;
 	
 	public static void menuGestionHotel() {
 		System.out.println("1. Álta de hotel\n2. Añadir habitación\n0. Salir");
